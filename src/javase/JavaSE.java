7f85f4eef23e71152fbdfd6f7522b0d9f823e9c9
michel adriano medeiros <michel.medeiros@ti.rc.sp.gov.br>
2018-11-20 01:21:02 -0200

    Ex1

---
+++ src/javase/JavaSE.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javase;
+
+import javase.ex1.Exemplo1;
+
+/**
+ *
+ * @author Michel
+ */
+public class JavaSE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Exemplo1 e = new Exemplo1();
+        e.setVisible(true);
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

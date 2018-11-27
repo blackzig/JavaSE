9ea904e686e78480d690f526e56becfa547bdf1c
michel adriano medeiros <michel.medeiros@ti.rc.sp.gov.br>
2018-11-26 22:15:44 -0200

    Ex1_2

--- src/javase/JavaSE.java
+++ src/javase/JavaSE.java
@@ -5,7 +5,7 @@
  */
 package javase;
 
-import javase.ex1.Exemplo1_1;
+import javase.ex1.Exemplo1_2;
 
 /**
  *
@@ -17,7 +17,7 @@ public class JavaSE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Exemplo1_1 e = new Exemplo1_1();
+        Exemplo1_2 e = new Exemplo1_2();
         e.setVisible(true);
     }
     
